82c45aaead51ba0ee97be79ae347fa50e91e7c92
Kennett Ramirez <kennett1519@gmail.com>
2025-03-05 15:32:36 -0500

    Actualizar el archivo main para el manejo de las rutas con los endpoints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routes.leyendas import router as leyenda_router
 from app.database import init_db
 from fastapi.staticfiles import StaticFiles
 
 #Inicializamos la base de datos
 init_db()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app = FastApi()
 app.add_middleware(
@@ -15,3 +19,6 @@ app.add_middleware(
   allow_methods=["*"], #Permite los métodos GET, POST, PUT, DELETE
   allow_headers=["*"],
 )
+app.include_router(leyenda_router)
+
+app.mount("/static", StaticFiles(directory=UPLOAD_DIR), name="static")
